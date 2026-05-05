0ed3e13a8e509779798dba51d53b1182e9a77ad0
Mr G <nobitamfatehii143@gmail.com>
2026-05-05 15:46:52 +0100

    VVIP Backend Final Fix

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const dbURI = "mongodb+srv://nobitamfatehii143_db_user:Cra6NpvAXSiA12DG@cluster0.4gbauam.mongodb.net/vvip_db?retryWrites=true&w=majority";
+
+mongoose.connect(dbURI)
+  .then(() => console.log("VVIP Sales Database Connected! 🟢"))
+  .catch((err) => console.log("DB Error: ", err));
+
+app.get('/', (req, res) => {
+  res.send("Mr. G ka Backend Live Hai! 🚀");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
